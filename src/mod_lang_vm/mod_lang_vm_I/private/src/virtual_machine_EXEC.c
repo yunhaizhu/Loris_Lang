aea9207a18ac2bdc553fab3f5a87d7968c623941
yunhai <yunhaia2@gmail.com>
2023-05-25 12:31:16 +0800

    fix heap after free issue in VAR L clean.

--- src/mod_lang_vm/mod_lang_vm_I/private/src/virtual_machine_EXEC.c
+++ src/mod_lang_vm/mod_lang_vm_I/private/src/virtual_machine_EXEC.c
@@ -1060,7 +1060,7 @@ STD_CALL static inline std_void_t inline_execute_code_VAR_L_CLEAN(environment_vm
     fp_index = (std_int_t) (*Fp - Codes[*Pc].i_operand_ex);
     object = Stack[fp_index];
 
-#if FAST_VAR_ENABLE
+#if FAST_VAR_ENABLE1
     const ownership_object_t *owner_object = get_owner_value_object(object);
     if (get_owner_value_type(owner_object->fast_value) == OWNER_TYPE_NUMBER) {
         return_owner_value_object_symbol(vm, object);
